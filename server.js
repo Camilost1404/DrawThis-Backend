aa8f415f9076c59cec8280f204eb470f48fe1a22
Camilost1404 <camitorres1404@gmail.com>
2021-05-21 17:56:50 -0500

    Configuración final

--- server.js
+++ server.js
@@ -1,5 +1,38 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
-const socket = require('socket.io')(server);
+const socketIo = require('socket.io')(server);
 
+
+socketIo.on('connection', (socket) => {
+
+    //generar id cada que una persona se conecte
+    const idHandShake = socket.id;
+
+    const { nameRoom } = socket.handshake.query;
+
+    console.log(`Dispositivo ${idHandShake} se unió a ${nameRoom}`);
+
+    // Unirlos a un grupo
+    socket.join();
+
+    socket.on('event', (res) => {
+
+        const data = res;
+        console.log(data);
+
+        socket.to(nameRoom).emit('event', data);
+    })
+
+
+})
+
+app.set('port', process.env.PORT || 3000);
+
+if (process.env.NODE_ENV !== 'test') {
+    app.listen(app.get('port'), () => {
+        console.log('Server on port ' + app.get('port') + ' on dev');
+    });
+}
+
+module.exports = app;
