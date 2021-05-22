c7e06dc5833f0244e594086872cc61b9516f3b23
Camilost1404 <camitorres1404@gmail.com>
2021-05-22 16:48:41 -0500

    Configuración final

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 app.use(cors());
 const options = {
     cors: {
-        origin: 'https://powerful-tor-71258.herokuapp.com/',
+        origin: 'https://powerful-tor-71258.herokuapp.com',
     },
 };
 
