34e9447049d17b691bf3a70fde73d8bedc13208b
Camilost1404 <camitorres1404@gmail.com>
2021-05-26 21:33:14 -0500

    Configuración final

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const options = {
 };
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:5000"); 
+    res.header("Access-Control-Allow-Origin", "*"); 
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method, Access-Control-Allow-Credentials');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
